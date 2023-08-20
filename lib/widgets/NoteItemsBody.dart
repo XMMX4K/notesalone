b0068a568282e5d67c98cdd6a2944bb9c8854a04
XMMX4K <mohamedelsharkawy208@gmail.com>
2023-08-20 10:05:09 +0300

    create custtom container and textfeild for Msheet

--- lib/widgets/NoteItemsBody.dart
+++ lib/widgets/NoteItemsBody.dart
@@ -3,38 +3,41 @@ import 'package:notes/helper/Colors.dart';
 import 'package:notes/widgets/CusttomText.dart';
 
 class NoteItemsBody extends StatelessWidget {
-  const NoteItemsBody({
-    super.key,
-  });
+  const NoteItemsBody({required this.onttap});
 
+  final void Function()? onttap;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20),
-      child: Container(
-        decoration: BoxDecoration(
-            color: MColor, borderRadius: BorderRadius.all(Radius.circular(32))),
-        width: MediaQuery.of(context).size.width,
-        height: 200,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ListTile(
-                title: CusttomText(text: 'Flutter Tips'),
-                subtitle: CusttomText(text: 'Find Your Way '),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.delete,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                )),
-            Padding(
-              padding: const EdgeInsets.only(left: 150),
-              child: CusttomText(text: '22/25/2250'),
-            ),
-          ],
+      child: GestureDetector(
+        onTap: onttap,
+        child: Container(
+          decoration: BoxDecoration(
+              color: MColor,
+              borderRadius: BorderRadius.all(Radius.circular(32))),
+          width: MediaQuery.of(context).size.width,
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ListTile(
+                  title: CusttomText(text: 'Flutter Tips'),
+                  subtitle: CusttomText(text: 'Find Your Way '),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete,
+                      size: 50,
+                      color: Colors.black,
+                    ),
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(left: 150),
+                child: CusttomText(text: '22/25/2250'),
+              ),
+            ],
+          ),
         ),
       ),
     );
