7810f9f7432f442a5aed50b82bb94ced49ed3a5b
XMMX4K <mohamedelsharkawy208@gmail.com>
2023-08-22 08:51:06 +0300

    add noteCubit Provider

--- lib/cubits/AddCubit/addCubit.dart
+++ lib/cubits/AddCubit/addCubit.dart
@@ -1,9 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/cubits/AddCubit/addState.dart';
+import 'package:notes/helper/Colors.dart';
 import 'package:notes/models/note_model.dart';
 
 class addcubit extends Cubit<AddNoteState> {
   addcubit() : super(addintail());
 
-  addnote(notemodel note) {}
+  addnote(notemodel note) async {
+    try {
+      var notebox = Hive.box(kmainbox);
+      emit(addSucsses());
+      await notebox.add(note);
+    } catch (e) {
+      emit(addFail(e.toString()));
+    }
+  }
 }
