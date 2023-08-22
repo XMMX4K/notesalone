7c37afe2b6c191c1fd367fd1429295e81dc24030
XMMX4K <mohamedelsharkawy208@gmail.com>
2023-08-22 08:33:16 +0300

    create hive adapter in main

--- lib/cubits/AddCubit/addCubit.dart
+++ lib/cubits/AddCubit/addCubit.dart
@@ -1,6 +1,9 @@
-import 'package:notes/Cubits/Cubit/addState.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/AddCubit/addState.dart';
+import 'package:notes/models/note_model.dart';
 
 class addcubit extends Cubit<AddNoteState> {
   addcubit() : super(addintail());
+
+  addnote(notemodel note) {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/helper/Colors.dart';
+import 'package:notes/models/note_model.dart';
 import 'package:notes/views/FirstView.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('$kmainbox');
+  Hive.registerAdapter(notemodelAdapter());
   runApp(NotesApp());
 }
 
